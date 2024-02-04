139e159ff945af4fb0654d54900d26bd495c8165
Maite Baida <maitemsabaida@gmail.com>
2024-02-04 15:23:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
