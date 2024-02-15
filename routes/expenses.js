9bc879129c755582ded630497d9467a925b449c3
Maite Baida <maitemsabaida@gmail.com>
2024-02-14 17:03:08 -0700

    user post request created, tested and running, might need to change route later

--- routes/expenses.js
+++ routes/expenses.js
@@ -2,7 +2,5 @@ const express = require('express');
 const router = express.Router();
 const expensesController = require ('../controllers/expenses-controller')
 
-// router 
-// .get('/', expensesController.userExpenses);
 
 module.exports = router;
